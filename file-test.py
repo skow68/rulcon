564d4a4f9e0b96578a8d909ad82580c597ff9ccf
Sławomir Kowalczyk <slawomirkow@gmail.com>
2024-02-16 21:17:59 +0100

    add file-test.py

--- file-test.py
+++ file-test.py
@@ -1,1 +1,6 @@
-#!/usr/bin/env python3
+import csv
+
+with open('./csv/local2.csv', 'r') as csv_file:
+    reader = csv.reader(csv_file)
+    input_rules = [row for row in reader]
+print(input_rules)
