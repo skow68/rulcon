3310d5e07a12de828c3808d3e249a8f525ffc6a9
Sławomir Kowalczyk <slawomirkow@gmail.com>
2024-03-30 21:58:38 +0100

    add processing tcp/udp ports at entry

--- regool/input_data.py
+++ regool/input_data.py
@@ -1,13 +1,13 @@
 import os
 import sys
-import regool.rgerrors as err
-
+import rgerrors as err
+ 
 # import ipaddress
-from regool.logger_setup import logger
+from logger_setup import logger
 import re
 import csv
-
-
+ 
+ 
 def get_rules(file_name):
     """
     Obsługa danych wejściowych zawierających reguły do wprowadzenia w formacie src_name-src_ip-dst_name-dst_ip-dst_port.
@@ -52,10 +52,10 @@ def get_rules(file_name):
                     or not validate_ip(row[1])
                     or not validate_fqdn(row[2])
                     or not validate_ip(row[3])
-                    or not validate_port(row[4])
+                    or not validate_service(row[4])
                 ):
                     raise err.EntryDataError(
-                        f"Invalid format of IP address in entry data."
+                        f"Invalid format of host or network address in entry data."
                     )
                     sys.exit(1)
         return input_rules
@@ -63,8 +63,8 @@ def get_rules(file_name):
         logger.error(f"Can not find entry file: {file_name}")
         raise err.EntryDataError(f"Can not find entry file: {file_name}")
         sys.exit(1)
-
-
+ 
+ 
 def validate_ip(ip_address):
     """
     Args:
@@ -90,8 +90,8 @@ def validate_ip(ip_address):
     else:
         #logger.error(f"The IP address {ip_address} is invalid")
         return False
-
-
+ 
+ 
 def validate_fqdn(fqdn):
     """
     Simple validation for now. Not realu fqdn.
@@ -104,18 +104,119 @@ def validate_fqdn(fqdn):
     else:
         #logger.error(f"Address {fqdn} is invalid")
         return False
-
-
-def validate_port(port):
+ 
+ 
+def validate_service(portstr):
     """
-    Validates format of service ports. It can be: 80,443,samba, 7000-8000
+    Validates format of service ports. It can be: 80,443, 7000-8000, 53/udp, 9000-9010/udp
+    Nie tylko waliduje, ale od razu zamienia na nazwy service object zgodnie z konwencją:
+    ['tcp-80', 'tcp-443', 'tcp-7000-8000', 'udp-53', 'tcp-2001', 'tcp-2002', 'tcp-2003', 'udp-9000-9010/udp']
     """
-    ports = port.split(",")
-    pattern = r"^[a-zA-Z0-9]{1,6}$|^\d+-\d+$"
+    ports = portstr.split(",")
+    output = []
+    portlist = []
+    portlist_udp = []
+    #pattern = r"^[a-zA-Z0-9]{1,6}$|^\d+-\d+$"
+    idx = 0
     for p in ports:
-        p.strip()
+        p = p.strip()
+        pattern = r"^\d+$"
+        #65535
+        start = len(output)
         match = re.match(pattern, p)
-        if not match:
-            logger.error(f"The TCP port or service name {p} is invalid")
+        if match:
+            if int(p) < 65536:
+                output.append('tcp-' + p)
+                if int(p) > 1024:
+                    portlist.append(int(p))
+            else:
+                try:
+                    raise err.EntryDataError(f"UDP range not correct: {p}")
+                except err.EntryDataError:
+                    sys.exit(1)
+        idx += 1
+        pattern = r"^\d+-\d+$"
+        match = re.match(pattern, p)
+        if match:
+            parts = p.split('-')
+            if int(parts[0]) < 65536 and int(parts[1]) < 65536 and int(parts[0]) < int(parts[1]):
+                output.append('tcp-' + p)
+            else:
+                try:
+                    raise err.EntryDataError(f"UDP range not correct: {p}")
+                except err.EntryDataError:
+                    sys.exit(1)
+        pattern = r"^(\d+)\/udp$"
+        match = re.match(pattern, p)
+        if match:
+            if int(match.group(1)) < 65536:
+                output.append('udp-' + match.group(1))
+                portlist_udp.append(match.group(1))
+            else:
+                try:
+                    raise err.EntryDataError(f"UDP range not correct: {p}")
+                except err.EntryDataError:
+                    sys.exit(1)
+        pattern = r"^(\d+-\d+)\/udp$"
+        match = re.match(pattern, p)
+        if match:
+            pp = match.group(1)
+            parts = pp.split('-')
+            if int(parts[0]) < 65536 and int(parts[1]) < 65536 and int(parts[0]) < int(parts[1]):
+                output.append('udp-' + p)
+            else:
+                try:
+                    raise err.EntryDataError(f"UDP range not correct: {p}")
+                except err.EntryDataError:
+                    sys.exit(1)
+        stop = len(output)
+        if stop - start == 0:
+            logger.error(f"At least one of the TCP/UDP port is invalid")
             return False
-    return True
+    # wychwytywanie ciągów portów różniących się niewiele i które można zamienić na zakres
+    min_dif = 10 #różnica między portami, aby uznać ich podobnymi
+    min_nr_to_compress = 3 # min. liczba kolejnych podobnych portów, aby ich komprespwać do zakresu
+    portlist.sort()
+    dif = []
+    for i in range(0, len(portlist) - 1):
+        tab = portlist[i::1]
+        dif.append(int(tab[1]) - int(tab[0]))
+    pairs = []
+    print(dif)
+    x = 0
+    start = 0
+    stop = 0
+    rang = False
+    for i in range(0, len(dif)):
+        if dif[i] <= min_dif:
+            x += 1
+            if i == len(dif) - 1: #obsługa warunku brzegowego; gdy porty są na samym końcu
+                stop = i + 2
+                start = i - x + 1
+                pairs.append([start,stop])
+                print(pairs)
+        else:
+            x = 0
+        if x >= min_nr_to_compress - 1:
+            start = i - x + 1
+            rang = True
+        elif x < min_nr_to_compress - 1 and rang:
+            stop = i - x + 1
+            pairs.append([start,stop])
+            rang = False
+            print(f'pairs: {pairs}')
+    for pair in pairs:
+        print(f'portlist: {portlist[int(pair[0]):int(pair[1])]}')
+        slice_of_portlist = portlist[int(pair[0]):int(pair[1])]
+        for s in slice_of_portlist:
+            output.remove('tcp-' + str(s))
+        rangetcp = 'tcp-' + str(portlist[pair[0]]) + '-' + str(portlist[pair[1]-1])
+        print(rangetcp)
+        output.append(rangetcp)
+    return output
+ 
+if __name__ == "__main__":
+    input = '1500, 1502, 1503, 80,443, 7000-8000, 53/udp, 2000, 2010, 2008, 8010, 8020, 8030, 9000-9010/udp'
+    #input = '80, 7000-8000'
+    validated = validate_service(input)
+    print(validated)
